6a86d5cf906088427b1658bd3315e46847bf2ae6
Fish <109036036+Peixe2b@users.noreply.github.com>
2024-10-07 10:47:54 -0300

    Update main.py

--- mandaw/main.py
+++ mandaw/main.py
@@ -10,38 +10,20 @@ from mandaw.utils.softwareRenderer import SoftwareRenderer
 
 class Mandaw:
     def __init__(self, title="Mandaw", width=800, height=600):
-        sdl2.ext.init()
         self.window = Window(title, width, height)
-        self.sprite_renderer: SoftwareRenderer = SoftwareRenderer(self.window, self)
         self.gameTime = GameTime()
         self.running: bool = True
         self.__initialize()
-        self.window.show()
             
     def update(self):
-        self.__inputs()
+        self.window.gameLoop()
         self.gameTime.updateTime()
         self.world.process()
 
     def set_background_color(self, color: Color):
         self.window.bg_color = color
 
-    def __initialize(self): # Initalize all configurations 
-        self.window.bg_color = self.bg_color
-        self.world.width = self.width
-        self.world.height = self.height
-        # Set icon
+    def __initialize(self):
         __path: str = os.path.dirname(os.path.abspath(__file__))
         self.icon: str = os.path.join(__path, "./assets/mandaw.png")
-        
-        # Show window
         self.window.show()
-
-    def __inputs(self):
-        events = sdl2.ext.get_events()
-        for event in events:
-            if event.type == sdl2.SDL_QUIT:
-                quit()
-            if event.type == sdl2.SDL_KEYDOWN:
-                if event.key.keysym.sym == sdl2.SDLK_ESCAPE:
-                    quit()
